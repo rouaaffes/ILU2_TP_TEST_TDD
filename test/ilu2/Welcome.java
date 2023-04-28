cba4780c7d397ed506b5c180dcb530a2bff15b65
Roua affes <Roua affes@192.168.1.3>
2023-04-28 23:38:59 +0200

    ITER_1

--- test/ilu2/Welcome.java
+++ test/ilu2/Welcome.java
@@ -2,4 +2,17 @@ package ilu2;
 
 public class Welcome {
 
+	public static String welcome(String input) {
+		if (input == null) {
+			throw new IllegalArgumentException("Le chaine de carctère du nom  ne peut pas être null");
+		}
+		// Vérifier que le paramètre n'est pas vide
+		if (input.isEmpty()) {
+			throw new IllegalArgumentException("La chaine de carctère du nom ne peut pas être vide");
+		}
+		String name = input.substring(0, 1).toUpperCase() + input.substring(1);
+		// Retourner la chaîne "Hello, <nom>"
+		return "Hello, " + name;
+	}
+
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -1,14 +1,24 @@
 package ilu2;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_EX1() {
+		assertEquals("Hello, Bob", Welcome.welcome("bob"));
+		assertEquals("Hello, Martin", Welcome.welcome("Martin"));// cas premiere lettre déja en majuscule
+		assertEquals("Hello, Élodie", Welcome.welcome("élodie"));// test lettre avec accent
+		assertEquals("Hello, Jean-baptiste", Welcome.welcome("jean-baptiste"));
+		assertThrows(IllegalArgumentException.class, () -> {
+			Welcome.welcome("");// cas chaine vide
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			Welcome.welcome(null); // cas paramètre null
+		});
 	}
 
 }
